3d1c491fc8dda0844cb0577868cd27031df8ed13
ollienye50 <olivianye50@gmail.com>
2022-10-14 21:31:15 +1100

    changed things in the assets folder

---
+++ Assets/MicroGames/Script_SearchWeapons/SearchWeapons.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchWeapons : MonoBehaviour
+{
+    //make the players cursor appear on the screen
+
+    //make the objects clickable
+
+    //assign the correct and incorrect tools with a simple print of wrong and right
+
+    //when clicking the wrong one, a square covers the screen displaying game over or life lost (substitue for the real life system screen)
+
+    // when clicking the right one, a square covers the screen displaying correct and next level (substitute for the real next level screen)
+    
+    //event systems might work if the weapon is random every game
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
