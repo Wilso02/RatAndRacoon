7ee603468181e659aa50564d206caa84f9b0f067
RileySmith926 <99451494+RileySmith926@users.noreply.github.com>
2022-11-05 15:05:02 +1100

    think microgame finished :) (mostly)

--- Assets/Scripts&Materials/ThinkScripts/Think.cs
+++ Assets/Scripts&Materials/ThinkScripts/Think.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class Think : MonoBehaviour
 {
-    Vector2 randomPosition;
-    public float xRange = 3f;
-
-    public float yRange = 3f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        float xPosition = Random.Range(0 - xRange, 0 + xRange);
-        float yPosition = Random.Range(0 - yRange, 0 + yRange);
-        randomPosition = new Vector2(xPosition, yPosition);
-        transform.position = randomPosition;
+        
         GetComponent<Renderer>().material.color = Color.red;
     }
 
