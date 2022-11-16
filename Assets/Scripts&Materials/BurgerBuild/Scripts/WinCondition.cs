beeb10259ce4f1f427b1ad21ebf854b33a335bfc
SAE1\1026372 <1026372@student.sae.edu.au>
2022-11-16 16:14:37 +1100

    worked onburger build

--- Assets/Scripts&Materials/BurgerBuild/Scripts/WinCondition.cs
+++ Assets/Scripts&Materials/BurgerBuild/Scripts/WinCondition.cs
@@ -6,11 +6,15 @@ public class WinCondition : MonoBehaviour
 {
     public static float WinCount;
 
-    void Update()
+    public static void WinCheck()
     {
         if(WinCount == 5)
         {
             print("you win!");
+        }
+        else
+        {
+            print("win count is now " + WinCondition.WinCount);
         }
     }
 }
--- Assets/Scripts&Materials/BurgerBuild/Scripts/bunsLockIn.cs
+++ Assets/Scripts&Materials/BurgerBuild/Scripts/bunsLockIn.cs
@@ -4,38 +4,27 @@ using UnityEngine;
 
 public class bunsLockIn : MonoBehaviour
 {
-
     GameObject Buns;
     GameObject bunsPlaceholder;
     
-   
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider BunSnap)
     {
         if (BunSnap.tag == "buns")
         {
             Debug.Log("working");
             //move the Buns object to bunsPlaceholder
-            Buns.transform.position = new Vector3(0f, 10.44f, 0f);
+            BunSnap.transform.position = new Vector3(0f, 10.44f, 0f);
             //Buns.transform.position = bunsPlaceholder.transform.position;
 
             //stop the Buns object from being able to move
-            
+            //Destroy(Buns.GetComponent<Dragger>());
+            WinCondition.WinCount++;
+            WinCondition.WinCheck();
+            Destroy(Buns);
+            Destroy(bunsPlaceholder);
+
+
             //Add to the win condition counter
-            WinCondition.WinCount =+ 1;
-            print("win count is now" + WinCondition.WinCount);
         }
     }
 
