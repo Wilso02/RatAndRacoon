5f72be550faeb02e05061299bb89aa88c7744852
Lith_VV <darkninja65786@gmail.com>
2022-11-23 21:17:54 +1100

    main menu has been add

---
+++ Assets/Shared/Scripts/main_menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class main_menu : MonoBehaviour
+{
+    public void startgame()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene("Credits");
+
+    }
+
+
+    public void QuitGame()
+
+    {
+        print("game has been quit");
+        Application.Quit();
+    }
+}
