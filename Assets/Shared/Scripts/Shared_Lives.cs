bca50f0c13e7390c671be3ef09416218ae677fd5
ollienye50 <olivianye50@gmail.com>
2022-12-10 20:33:02 +1100

    fixed text

--- Assets/Shared/Scripts/Shared_Lives.cs
+++ Assets/Shared/Scripts/Shared_Lives.cs
@@ -15,107 +15,73 @@ public class Shared_Lives : MonoBehaviour
 
     void OnEnable()
     {
-
-        
         Shared_EventManager.LostTheGame += DecreaseLives;
-        
     }
 
     private void OnDisable()
     {
-
-        
         Shared_EventManager.LostTheGame -= DecreaseLives;
-        
     }
 
     private void Awake()
     {
-
         //set int playerLives to obtain the pref int
         playersLives = PlayerPrefs.GetInt("Lives");
-        PlayerPrefs.SetInt("Lives", 3);
+        //PlayerPrefs.SetInt("Lives", 3);
     }
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
         if(playersLives == 3)
         {
-
             ThreeLivesDisplayed();
-
         }
-
         else if(playersLives == 2)
         {
-
             TwoLivesDisplayed();
-
         }
-
         else if(playersLives == 1)
         {
-
             OneLivesDisplayed();
-
         }
-
-
     }
 
     void ThreeLivesDisplayed()
     {
-
         livesUI_1.SetActive(true);
         livesUI_2.SetActive(true);
         livesUI_3.SetActive(true);
-
     }
 
     void TwoLivesDisplayed()
     {
-
         livesUI_1.SetActive(true);
         livesUI_2.SetActive(true);
         livesUI_3.SetActive(false);
-        
     }
 
     void OneLivesDisplayed()
     {
-
         livesUI_1.SetActive(true);
         livesUI_2.SetActive(false);
         livesUI_3.SetActive(false);
-
     }
-
-
-
 
     void DecreaseLives()
     {
-
         playersLives--;
         Debug.Log("The game was lost, player lives decreased to "+playersLives);
         PlayerPrefs.SetInt("Lives", playersLives);
         LoseScreen();
-
     }
 
     void LoseScreen()
     {
-
         if(playersLives <= 0)
         {
-
             PlayerPrefs.SetInt("Lives", 3);
             SceneManager.LoadScene("LoseScene");
-
         }
-
     }
-   
 }
